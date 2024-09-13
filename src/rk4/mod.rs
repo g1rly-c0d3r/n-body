e2fdb0d60c5f4986909e695451c360ed755bfea6
g1rly-c0d3r <girly_c0d3r@aol.com>
2024-09-13 10:27:45 -0500

    figuring out ode int

--- src/rk4/mod.rs
+++ src/rk4/mod.rs
@@ -3,8 +3,27 @@ mod rk4 {
 use std::result::Result;
 use std::vec::Vec;
 
-    fn itterate(dydt:fn(f64, Vec<f64>) -> Vec<f64>, Y_n:&[f64], t:f64, h:f64, m:const u8) -> Result< Vec<f64> , &'static str> {
-        let mut k = [[0f64; 4]; m];
+    fn itterate(dydt:fn(f64, Vec<f64>) -> Vec<f64>, Y_n:&[f64], t:f64, h:f64, m:usize) -> Result< Vec<f64> , &'static str> {
+        let mut k: Vec<Vec<f64>> = Vec::new();
+
+        for i in 0..4{
+            k.push(Vec::new());
+        }
+
+        let step = dydt(t, Y_n.to_vec());
+        for i in 0..m{
+            k[0].push(h * step[i]);
+        }
+
+        let step = Y_n.to_vec().iter().zip(k[0].iter()).map(|(x,y)| x+y).collect();
+        for i in 0..m{
+            k[1].push(
+                h * dydt(t + h/2f64, step[i]);
+            )
+        }
+
+
+        
         
         Ok(vec![1.0f64])
     }
