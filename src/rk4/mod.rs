8365c9d8ce91ad89cd5e6747d00deeb02ec74945
g1rly-c0d3r <girly_c0d3r@aol.com>
2024-09-15 18:24:31 -0500

    finished itterate

--- src/rk4/mod.rs
+++ src/rk4/mod.rs
@@ -1,9 +1,7 @@
-
 mod rk4 {
-use std::result::Result;
 use std::vec::Vec;
 
-    fn itterate(dydt:fn(f64, Vec<f64>) -> Vec<f64>, Y_n:&[f64], t:f64, h:f64, m:usize) -> Result< Vec<f64> , &'static str> {
+    fn itterate(dydt:fn(f64, Vec<f64>) -> Vec<f64>, Y_n:&[f64], t:f64, h:f64, m:usize) -> Vec<f64> {
         let mut k: Vec<Vec<f64>> = Vec::new();
 
         for i in 0..4{
@@ -15,17 +13,37 @@ use std::vec::Vec;
             k[0].push(h * step[i]);
         }
 
-        let step = Y_n.to_vec().iter().zip(k[0].iter()).map(|(x,y)| x+y).collect();
+        let step = dydt(t + h/2f64, Y_n.to_vec().iter().zip(k[0].iter()).map(|(x,y)| (x+y)/2f64).collect());
         for i in 0..m{
             k[1].push(
-                h * dydt(t + h/2f64, step[i]);
-            )
+                h * step[i]
+            );
         }
 
+        let step = dydt(t + h/2f64, Y_n.to_vec().iter().zip(k[1].iter()).map(|(x,y)| (x+y)/2f64 ).collect());
+        for i in 0..m{
+            k[2].push(
+                h * step[i]
+            );
+        }
+        
+        let step = dydt(t + h, Y_n.to_vec().iter().zip(k[2].iter()).map(|(x,y)| x+y).collect());
+        for i in 0..m{
+            k[3].push(
+                h * step[i]
+            );
+        }
 
-        
-        
-        Ok(vec![1.0f64])
+        let a: Vec<_> = k[1].iter().zip(k[2].iter()).map(|(x,y)| 2f64*(x+y)).collect();
+        let b: Vec<_> = k[0].iter().zip(k[3].iter()).map(|(x,y)| (x+y)).collect();
+        let c: Vec<_> = a.iter().zip(b.iter()).map(|(x,y)| (x+y)/6f64).collect();
+
+        Y_n.iter().zip(c.iter()).map(|(x,y)| x+y).collect()
     }
 
+
+    fn RK4() -> {
+
+    }
 }
+
